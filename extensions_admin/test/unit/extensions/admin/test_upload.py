edc4d708d55132f3b62c54784cff86d9b243899d
Chris Duryee <cduryee@redhat.com>
2014-06-22 19:47:04 -0400

    Fix broken unit test
    I missed something in PR #9, fixing.

--- extensions_admin/test/unit/extensions/admin/test_upload.py
+++ extensions_admin/test/unit/extensions/admin/test_upload.py
@@ -34,21 +34,21 @@ class TestGenerateUnitKeyAndMetadata(unittest.TestCase):
     def test_with_cirros(self):
         unit_key, metadata = self.command.generate_unit_key_and_metadata(data.cirros_img_path)
 
-        self.assertEqual(unit_key, {'image_checksum': '64d7c1cd2b6f60c92c14662941cb7913',
-                                    'image_size': 13167616,
-                                    'image_filename': 'cirros-0.3.2-x86_64-disk.img'})
-        self.assertEqual(metadata, {'image_protected': True})
+        self.assertEqual(unit_key, {'image_checksum': '64d7c1cd2b6f60c92c14662941cb7913'})
+        self.assertEqual(metadata, {'image_protected': True,
+                                    'image_filename': 'cirros-0.3.2-x86_64-disk.img',
+                                    'image_size': 13167616})
 
     def test_with_cirros_and_metadata(self):
         unit_key, metadata = self.command.generate_unit_key_and_metadata(data.cirros_img_path,
                                                                          image_min_ram=1024,
                                                                          image_name="fake name")
 
-        self.assertEqual(unit_key, {'image_checksum': '64d7c1cd2b6f60c92c14662941cb7913',
-                                    'image_size': 13167616,
-                                    'image_filename': 'cirros-0.3.2-x86_64-disk.img'})
+        self.assertEqual(unit_key, {'image_checksum': '64d7c1cd2b6f60c92c14662941cb7913'})
         self.assertEqual(metadata, {'image_protected': True, 'image_min_ram': 1024,
-                                    'image_name': "fake name"})
+                                    'image_name': "fake name",
+                                    'image_filename': 'cirros-0.3.2-x86_64-disk.img',
+                                    'image_size': 13167616})
 
     def test_file_does_not_exist(self):
         self.assertRaises(IOError, self.command.generate_unit_key_and_metadata, '/a/b/c/d')
