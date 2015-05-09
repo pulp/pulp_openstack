a3d0898c8c6531ea0593707f178e1ac8236360c8
Randy Barlow <rbarlow@redhat.com>
2015-05-09 19:32:56 -0400

    pulp-dev.py calls manage_setup.pys for you.

--- pulp-dev.py
+++ pulp-dev.py
@@ -5,6 +5,9 @@ import optparse
 import os
 import sys
 
+from pulp.devel import environment
+
+
 WARNING_COLOR = '\033[31m'
 WARNING_RESET = '\033[0m'
 
@@ -18,6 +21,8 @@ DIRS = ('/var/lib/pulp/published/openstack/web',)
 
 # Standard directories
 DIR_PLUGINS = '/usr/lib/pulp/plugins'
+
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 LINKS = (
     ('plugins/etc/httpd/conf.d/pulp_openstack.conf', '/etc/httpd/conf.d/pulp_openstack.conf'),
@@ -93,6 +98,9 @@ def getlinks():
 
 
 def install(opts):
+    # Install the packages in developer mode
+    environment.manage_setup_pys('install', ROOT_DIR)
+
     warnings = []
     create_dirs(opts)
     # Ensure the directory is owned by apache
@@ -119,6 +127,8 @@ def uninstall(opts):
             continue
         os.unlink(dst)
 
+    # Uninstall the packages
+    environment.manage_setup_pys('uninstall', ROOT_DIR)
     return os.EX_OK
 
 
@@ -159,7 +169,6 @@ def _create_link(opts, src, dst):
                   % (dst, src, e)
             return msg
 
-# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     # TODO add something to check for permissions
